b1b05df9f8d2535feb310f8379b2f0fa7a3288e9
华清松 <821034742@qq.com>
2019-09-03 15:35:02 +0800

    增加测试接口

--- src/main/java/vip/qsos/ktorm/localupload/controller/TestController.kt
+++ src/main/java/vip/qsos/ktorm/localupload/controller/TestController.kt
@@ -1,12 +1,14 @@
 package vip.qsos.ktorm.localupload.controller
 
 import io.swagger.annotations.ApiOperation
+import me.liuwj.ktorm.dsl.delete
+import me.liuwj.ktorm.dsl.deleteAll
+import me.liuwj.ktorm.dsl.eq
+import me.liuwj.ktorm.dsl.update
 import me.liuwj.ktorm.entity.add
 import me.liuwj.ktorm.entity.findAll
-import me.liuwj.ktorm.entity.findById
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RestController
+import me.liuwj.ktorm.entity.findOne
+import org.springframework.web.bind.annotation.*
 import vip.qsos.ktorm.localupload.entity.Employees
 import vip.qsos.ktorm.localupload.entity.IEmployee
 import vip.qsos.ktorm.localupload.entity.TableEmployee
@@ -16,49 +18,69 @@ import vip.qsos.ktorm.util.Result
 class TestController : BaseController() {
 
     @PostMapping("/add")
-    @ApiOperation(value = "测试接口", notes = "仅用于框架测试连通性")
+    @ApiOperation(value = "测试接口")
     fun add(): Result<TableEmployee> {
         val e = IEmployee()
         e.name = "Name"
-        e.job = "JOB"
-        e.head = "http://blog.qsos.vip/upload/2018/11/ic_launcher20181225044818498.png"
+        e.job = "JOB2"
+        e.head = "http://img.sccnn.com/bimg/338/42729.jpg"
         e.managerId = 1
-        var id: Int? = null
-        Employees.add(e).let {
-            id = it
-        }
+        val result = TableEmployee(Employees.add(e), e.name, e.managerId, e.job, e.head)
         println("新增1条数据")
-        val result = TableEmployee(id, e.name, e.managerId, e.job, e.head)
         return Result<TableEmployee>().result(result)
     }
 
+    @DeleteMapping("/delete")
+    @ApiOperation(value = "测试接口")
+    fun delete(): Result<String> {
+        Employees.delete { it.name.eq("JOB2") }
+        println("删除1条数据")
+        return Result<String>().result("删除成功")
+    }
+
+    @DeleteMapping("/clear")
+    @ApiOperation(value = "测试接口")
+    fun clear(): Result<String> {
+        Employees.deleteAll()
+        println("清除数据")
+        return Result<String>().result("清除数据")
+    }
+
+    @PutMapping("/update")
+    @ApiOperation(value = "测试接口")
+    fun update(@RequestBody em: IEmployee): Result<IEmployee> {
+        Employees.update {
+            it.name to em.name
+            it.job to em.job
+            it.managerId to em.managerId
+
+            where {
+                it.id eq em.id!!
+            }
+        }
+        println("更新数据")
+        return Result<IEmployee>().result(em)
+    }
+
     @GetMapping("/list")
-    @ApiOperation(value = "测试接口", notes = "仅用于框架测试连通性")
+    @ApiOperation(value = "测试接口")
     fun findAll(): Result<List<TableEmployee>> {
-        val e = IEmployee()
-        e.name = "Name"
-        e.job = "JOB"
-        e.head = "http://blog.qsos.vip/upload/2018/11/ic_launcher20181225044818498.png"
-        e.managerId = 1
-        Employees.add(e)
-        println("新增1条数据")
-
         val testList = Employees.findAll().map { TableEmployee.trans(it) }
-
         println("查询到数据 ${testList.size} 条")
         return Result<List<TableEmployee>>().result(testList)
     }
 
     @GetMapping("/one")
-    @ApiOperation(value = "测试接口", notes = "仅用于框架测试连通性")
+    @ApiOperation(value = "测试接口")
     fun findOne(): Result<TableEmployee> {
-        Employees.findById(1).let {
-            return if (it == null) {
-                Result<TableEmployee>().error(500, "无法找到")
-            } else {
-                TableEmployee.trans(it)
-                Result<TableEmployee>().result(TableEmployee.trans(it))
-            }
+        val one = Employees.findOne {
+            it.name eq "Name"
+        }
+        return if (one == null) {
+            Result<TableEmployee>().error(500, "无法找到")
+        } else {
+            TableEmployee.trans(one)
+            Result<TableEmployee>().result(TableEmployee.trans(one))
         }
 
     }
