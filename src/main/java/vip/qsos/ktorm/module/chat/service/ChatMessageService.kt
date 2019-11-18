dbc432584683a586042b5fb3bb5f785c1b770a78
华清松 <821034742@qq.com>
2019-11-18 17:33:02 +0800

    修改消息排序，增加消息撤回；

--- src/main/java/vip/qsos/ktorm/module/chat/service/ChatMessageService.kt
+++ src/main/java/vip/qsos/ktorm/module/chat/service/ChatMessageService.kt
@@ -34,8 +34,7 @@ class ChatMessageService @Autowired constructor(
     override fun getMessageListBySessionId(sessionId: Int): List<ChatMessageInfoBo> {
         val list: ArrayList<ChatMessageInfoBo> = arrayListOf()
         DBChatMessage.findList {
-            it.sessionId eq sessionId
-            it.cancelBack eq false
+            (it.sessionId eq sessionId) and (it.cancelBack eq false)
         }.map { msg ->
             getDBChatUserWithMessage(msg, list)
         }
--- src/main/java/vip/qsos/ktorm/module/file/entity/FileResourceBo.kt
+++ src/main/java/vip/qsos/ktorm/module/file/entity/FileResourceBo.kt
@@ -32,7 +32,7 @@ class FileResourceBo : IBo<TableFileResource> {
         return table?.let {
             FileResourceBo(
                     // FIXME 配置访问HOST
-                    url = "http://192.168.0.103:8085/" + table.url,
+                    url = "http://192.168.1.14:8085/" + table.url,
                     filename = table.filename,
                     type = table.type
             )
